ba4c60ce17e3bc33760032c13f047fb166bd8b7f
Arul Nanda <arulnanda@Aruls-MacBook-Air-2.local>
2025-10-15 19:07:26 +0530

    Initializing code

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("Hello world")
+from sklearn.decomposition import PCA
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-print("helloooooo")
+def Airline(airlinename):
+    df = pd.read_csv(f"{airlinename}")
+    #Year,Month,Stockprice omitted
+    x = df.iloc[:,3:]
+    #standardize
+    standard = StandardScaler()
+    x = standard.fit_transform(x)
+    #Index
+    pca = PCA(n_components=1)
+    pca.fit(x)
+    sensitivity_index = pca.transform(x)
+    
